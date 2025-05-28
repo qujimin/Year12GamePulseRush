8e7ff15042619c6da89599bc3a039cbc51811db6
qujimin <rafsil36@kingshigh.school.nz>
2025-05-29 10:04:19 +1200

    menuscreenv1

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game.tscn")
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Options.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
