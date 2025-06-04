3433039c79f54858f3e931d1b09a12d37c178e71
qujimin <rafsil36@kingshigh.school.nz>
2025-06-04 14:55:02 +1200

    menu_finale

--- main_menu.gd
+++ main_menu.gd
@@ -1,13 +1,12 @@
-extends Node2D
+extends Control
 
+var button_type = null
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://level_1.tscn")
 
-
-func _on_options_pressed() -> void:
-	get_tree().change_scene_to_file("res://Options.tscn")
-
+func _on_settings_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
