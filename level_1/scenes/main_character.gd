6a986918174650fcd19320dcbd8a0c0b22791cdc
qujimin <rafsil36@kingshigh.school.nz>
2025-06-12 14:57:40 +1200

    level_1_v3

--- level_1/scenes/main_character.gd
+++ level_1/scenes/main_character.gd
@@ -3,22 +3,21 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
-	#Add the gravity
+	# Apply gravity when not on the floor
 	if not is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y += gravity * delta  # Apply gravity to pull the character down
 
+	# Handle jump input
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y =JUMP_VELOCITY
+		velocity.y = JUMP_VELOCITY  # Set upward velocity only when jumping
 
-
+	# Handle horizontal movement
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
-
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
