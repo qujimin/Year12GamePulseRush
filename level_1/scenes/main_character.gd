d17492e928656d6b27124a360909423f3147bf6d
qujimin <rafsil36@kingshigh.school.nz>
2025-06-17 10:19:38 +1200

    level_1_v4

--- level_1/scenes/main_character.gd
+++ level_1/scenes/main_character.gd
@@ -1,14 +1,23 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
+@onready var sprite_2d = $Sprite2D
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
+	# Animations:
+	if (velocity.x > 1 or velocity.x < -1):
+		sprite_2d.animation = "running"
+	else:
+		sprite_2d.animation = "idle"
+		
+	
 	# Apply gravity when not on the floor
 	if not is_on_floor():
 		velocity.y += gravity * delta  # Apply gravity to pull the character down
+		sprite_2d.animation = "jumping"
 
 	# Handle jump input
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -19,6 +28,9 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, 12)
 
 	move_and_slide()
+
+	var isLeft = velocity.x < 0
+	sprite_2d.flip_h = isLeft
