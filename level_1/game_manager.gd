e19f671175bb1a6c34536b59bf99f16878d244a4
qujimin <rafsil36@kingshigh.school.nz>
2025-07-18 10:05:46 +1200

    level_v1_9.5

--- level_1/game_manager.gd
+++ level_1/game_manager.gd
@@ -1,7 +1,10 @@
 extends Node
 
+@onready var points_label: Label = %PointsLabel
+
 var points = 0
 
 func add_point():
 	points += 1
 	print(points)
+	points_label.text = "Points: " + str(points)
--- menu_scene/main_menu.gd
+++ menu_scene/main_menu.gd
@@ -3,10 +3,10 @@ extends Control
 var button_type = null
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_1.tscn")
+	get_tree().change_scene_to_file("res://level_1/level_1.tscn")
 
 func _on_settings_pressed() -> void:
-	get_tree().change_scene_to_file("res://options.tscn")
+	get_tree().change_scene_to_file("res://menu_scene/Options.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
