caf0e6b7e9f82e276643fd0e124e2aad4be79b2e
qujimin <rafsil36@kingshigh.school.nz>
2025-08-28 10:00:43 +1200

    level_v10_9.97

--- Interactibles/falling_spikes.gd
+++ Interactibles/falling_spikes.gd
@@ -1,25 +1,48 @@
 extends Node2D
-
 @export var speed = 160.0
 var current_speed = 0.0
-
 @onready var spawn_pos = global_position
+var has_dropped = false
+@onready var hitbox_bottom: float
+
+func _ready():
+	# Calculate the bottom of the PlayerDetect collision shape
+	var player_detect_area = $PlayerDetect
+	var collision_shape = player_detect_area.get_child(0)  # Get the CollisionShape2D
+	if collision_shape is CollisionShape2D:
+		var shape = collision_shape.shape
+		var shape_bottom = collision_shape.position.y + (shape.get_rect().size.y / 2)
+		hitbox_bottom = spawn_pos.y + shape_bottom
 
 func _physics_process(delta):
 	position.y += current_speed * delta
-
+	
+	# Stop when the spike's position reaches the bottom of its PlayerDetect area
+	if has_dropped and position.y >= hitbox_bottom:
+		current_speed = 0.0
+		position.y = hitbox_bottom  # Snap to exact position
 
 func _on_hitbox_area_entered(area):
 	if area.get_parent() is PlatformerController2D:
 		area.get_parent().take_damage(1)
-		queue_free()
+		# Reset after dealing damage
+		reset_spike()
 
-func _on_player_detect_area_entered(area):
-	if area.get_parent() is PlatformerController2D:
+func _on_player_detect_area_entered(area: Area2D) -> void:
+	if area.get_parent() is PlatformerController2D and not has_dropped:
+		has_dropped = true
 		$AnimationPlayer.play("Shake")
+		fall()
 
 func fall():
 	current_speed = speed
-	await get_tree().create_timer(3).timeout
+	await get_tree().create_timer(3.0).timeout
+	# Only reset if we haven't hit the player (and already reset)
+	if has_dropped:
+		reset_spike()
+
+func reset_spike():
 	position = spawn_pos
-	current_speed = 0
+	current_speed = 0.0
+	has_dropped = false
+	$AnimationPlayer.stop()  # Stop the shake animation
--- level_1/jumpad.gd
+++ level_1/jumpad.gd
@@ -1,16 +1,13 @@
-extends Node2D
-
+extends CharacterBody2D
 @export var launch_force: Vector2 = Vector2(0, -800)
 @onready var animated_sprite = $AnimatedSprite2D
-
 func _ready() -> void:
 	animated_sprite.play("idle")
 	$Area2D.body_entered.connect(_on_body_entered)
-
-func _on_body_entered(body) -> void:
+func onbody_entered(body) -> void:
 	if body is PlatformerController2D:
 		# Launch player
 		body.velocity += launch_force
-		
+
 		# Play jump animation and leave it there
 		animated_sprite.play("jump")
